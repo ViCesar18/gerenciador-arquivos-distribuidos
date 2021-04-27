b50e74fe7f5ee20fb83af6adb3aec07aeadcc705
Vinicius <vinicius.cesar@uel.br>
2021-04-27 15:09:27 -0300

    Testando exclusão mútua

--- host2/middleware.py
+++ host2/middleware.py
@@ -2,13 +2,39 @@ from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
 import shutil
 import os
+import time
     
+arquivo_ocupado = False
+
+
+def retorna_arquivo_ocupado():
+    if arquivo_ocupado:
+        return 1
+    
+    return 0
+
+
+def exclusao_mutua():
+    n_arquivos_ocupados = 1
+
+    print("Entrou na exclusao")
+    while n_arquivos_ocupados != 0:
+        n_arquivos_ocupados = 0
+        for middle_addr in MIDDLE_LIST:
+            with xmlrpc.client.ServerProxy(middle_addr) as proxy:
+                n_arquivos_ocupados += proxy.retorna_arquivo_ocupado()
+
+    print("Saiu da exclusao")
+
+
 def listar_arquivos_middle():
     return os.listdir("./dir")
 
 
 def listar_arquivos():
     arquivos = []
+
+    exclusao_mutua()
 
     for middle_addr in MIDDLE_LIST:
         with xmlrpc.client.ServerProxy(middle_addr) as proxy:
@@ -34,6 +60,8 @@ def criar_arquivo_middle(nome_arquivo):
 
 
 def criar_arquivo(nome_arquivo):
+    exclusao_mutua()
+
     lista_tamanhos = [{
         "middle_addr": "",
         "tamanho": buscar_tamanho_diretorio()
@@ -63,6 +91,10 @@ def renomear_arquivo_middle(nome_antigo, nome_novo):
 
 
 def renomear_arquivo(nome_antigo, nome_novo):
+    exclusao_mutua()
+
+    arquivo_ocupado = True
+
     for file in listar_arquivos_middle():
         if file == nome_antigo:
             renomear_arquivo_middle(nome_antigo, nome_novo)
@@ -75,6 +107,10 @@ def renomear_arquivo(nome_antigo, nome_novo):
                 if file == nome_antigo:
                     proxy.renomear_arquivo_middle(nome_antigo, nome_novo)
 
+    time.sleep(60)
+
+    arquivo_ocupado = False
+
 
 def excluir_arquivo_middle(nome_arquivo):
     arquivo = os.path.join("./dir", nome_arquivo)
@@ -82,6 +118,10 @@ def excluir_arquivo_middle(nome_arquivo):
 
 
 def excluir_arquivo(nome_arquivo):
+    exclusao_mutua()
+
+    arquivo_ocupado = True
+
     for file in listar_arquivos_middle():
         if file == nome_arquivo:
             excluir_arquivo_middle(nome_arquivo)
@@ -94,6 +134,8 @@ def excluir_arquivo(nome_arquivo):
                 if file == nome_arquivo:
                     proxy.excluir_arquivo_middle(nome_arquivo)
 
+    arquivo_ocupado = False
+
 
 BLUE_YELLOW   = "\033[1;33;44m"  
 RED  = "\033[1;31m"
@@ -101,7 +143,7 @@ CYAN  = "\033[1;36m"
 GREEN = "\033[1;32m"
 END_COLOR = "\033[m"
 
-print(BLUE_YELLOW + "Forneca o IP:PORT deste Middleware ->\n" + END_COLOR)
+""" print(BLUE_YELLOW + "Forneca o IP:PORT deste Middleware ->\n" + END_COLOR)
 IP = input(CYAN + "IP Deste Middleware -> " + END_COLOR)
 PORT = int(input(CYAN + "Porta Deste Middleware -> " + END_COLOR))
 
@@ -109,16 +151,22 @@ print(BLUE_YELLOW + "\nFornaca o IP:PORT dos outros Middlewares com o Formato IP
 print(GREEN + "Exemplo -> 127.0.0.1:8000\n" + END_COLOR)
 addr1 = input(CYAN + "Middleware 1 -> " + END_COLOR)
 addr2 = input(CYAN + "Middleware 2 -> " + END_COLOR)
-addr3 = input(CYAN + "Middleware 3 -> " + END_COLOR)
+addr3 = input(CYAN + "Middleware 3 -> " + END_COLOR) """
 
-MIDDLEWARE_1 = "http://" + addr1
+""" MIDDLEWARE_1 = "http://" + addr1
 MIDDLEWARE_2 = "http://" + addr2
-MIDDLEWARE_3 = "http://" + addr3
+MIDDLEWARE_3 = "http://" + addr3 """
+
+MIDDLEWARE_1 = "http://localhost:8100"
+MIDDLEWARE_2 = "http://localhost:8300"
+MIDDLEWARE_3 = "http://localhost:8400"
 
 MIDDLE_LIST = [MIDDLEWARE_1, MIDDLEWARE_2, MIDDLEWARE_3]
 
-server = SimpleXMLRPCServer((IP, PORT), allow_none=True)
-print(RED + f"\nEscutando a porta {PORT}..." + END_COLOR)
+server = SimpleXMLRPCServer(("localhost", 8200), allow_none=True)
+print(RED + f"\nEscutando a porta {8200}..." + END_COLOR)
+
+server.register_function(retorna_arquivo_ocupado, "retorna_arquivo_ocupado")
 server.register_function(listar_arquivos, "listar_arquivos")
 server.register_function(listar_arquivos_middle, "listar_arquivos_middle")
 server.register_function(buscar_tamanho_diretorio, "buscar_tamanho_diretorio")
